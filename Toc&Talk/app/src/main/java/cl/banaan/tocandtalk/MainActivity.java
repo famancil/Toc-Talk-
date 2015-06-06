139dca2df66014eed438f69652ab2316d57708b5
Felipe Mancilla <felipe.mancilla@alumnos.usm.cl>
2015-06-05 23:34:19 -0300

    Commit Inicial

---
+++ Toc&Talk/app/src/main/java/cl/banaan/tocandtalk/MainActivity.java
@@ -0,0 +1,54 @@
+package cl.banaan.tocandtalk;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView logo = (ImageView) findViewById(R.id.logo_start);
+        Button btn = (Button) findViewById(R.id.startbutton);
+
+        logo.setImageResource(R.mipmap.logo);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* Botón */
+                Uri uri = Uri.parse("http://stephenlb.github.io/webrtc-sdk/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
